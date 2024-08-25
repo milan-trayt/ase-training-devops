93c70a13cbd37ae0263d624d0bf656805addfc09
milan-trayt <milan@trayt.health>
2024-08-26 03:42:42 +0545

    splittr-backend cors

--- splittr-backend/src/app.js
+++ splittr-backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', authRoutes);
