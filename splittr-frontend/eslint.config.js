4634e2c603d5cfe7ade69962a95ce55ae8e34256
milan-trayt <milan@trayt.health>
2024-09-01 01:27:38 +0545

    pr check workflow

--- splittr-frontend/eslint.config.js
+++ splittr-frontend/eslint.config.js
@@ -20,7 +20,6 @@ export default [
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
-      'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
