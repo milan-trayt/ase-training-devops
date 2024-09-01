707e16fa4d14614b47e01b02cf273b6c7d01b7e6
milan-trayt <milan@trayt.health>
2024-09-02 03:46:51 +0545

    pr check workflow

--- terraform/modules/workflows/securitygroups/main.tf
+++ terraform/modules/workflows/securitygroups/main.tf
@@ -37,6 +37,15 @@ resource "aws_security_group" "sg_alb" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  egress {
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
   tags = {
     Name        = join("-", [var.stage, var.project, var.module, "SECGROUP-ALB"])
     Exposure    = "private"
