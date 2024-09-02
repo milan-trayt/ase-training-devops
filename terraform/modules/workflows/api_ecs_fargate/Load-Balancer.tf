df40175a2afdc156d9141d171ac563b7ae4e1fab
milan-trayt <milan@trayt.health>
2024-09-02 10:18:15 +0545

    pr check workflow

--- terraform/modules/workflows/api_ecs_fargate/Load-Balancer.tf
+++ terraform/modules/workflows/api_ecs_fargate/Load-Balancer.tf
@@ -12,11 +12,19 @@ resource "aws_lb" "LB" {
 
 resource "aws_alb_listener" "Listener" {
   load_balancer_arn = aws_lb.LB.id
-  port              = "80"
-  protocol          = "HTTP"
+  port              = "443"
+  protocol          = "HTTPS"
 
   default_action {
     target_group_arn = aws_lb_target_group.TG.id
     type             = "forward"
   }
+
+  certificate_arn = data.aws_acm_certificate.this.arn
+}
+
+data "aws_acm_certificate" "this" {
+  domain   = "dev.bibek65.tech"
+  most_recent = true
+  statuses = ["ISSUED"]
 }
