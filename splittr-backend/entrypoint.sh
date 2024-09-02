f461e1d2a3954c0a4017d649f8547594001ba8ec
milan-trayt <milan@trayt.health>
2024-09-02 11:01:42 +0545

    pr check workflow

--- splittr-backend/entrypoint.sh
+++ splittr-backend/entrypoint.sh
@@ -2,21 +2,21 @@
 
 set -e
 
+echo "RUN_SCHEMA_MIGRATION is set to $RUN_SCHEMA_MIGRATION"
+
 npm run generate
 
-echo $RUN_SCHEMA_MIGRATIONS
+secrets_manager_secrets=("${SECRET_PREFIX}-api")
+
+for secret in "${secrets_manager_secrets[@]}"; do
+  secret_value=$(aws secretsmanager get-secret-value --secret-id $secret --query SecretString --output text --region ${AWS_REGION})
+
+  while IFS="=" read -r key value; do
+    export "$key"="$value"
+  done < <(echo "$secret_value" | jq -r 'to_entries[] | "\(.key)=\(.value)"')
+done
 
 if [ "$RUN_SCHEMA_MIGRATIONS" = "true" ]; then
-  secrets_manager_secrets=("${SECRET_PREFIX}-api")
-
-  for secret in "${secrets_manager_secrets[@]}"; do
-    secret_value=$(aws secretsmanager get-secret-value --secret-id $secret --query SecretString --output text --region ${AWS_REGION})
-
-    while IFS="=" read -r key value; do
-      export "$key"="$value"
-    done < <(echo "$secret_value" | jq -r 'to_entries[] | "\(.key)=\(.value)"')
-  done
-
   if [ "$SECRET_PREFIX" = "dev" ]; then
     npm run migrate:dev
   else
