5e7384b3d714ec206a9a7ca119719c6ffdc12917
milan-trayt <milan@trayt.health>
2024-09-04 01:11:50 +0545

    fix(patch): initialize semver for terraform

---
+++ terraform/modules/workflows/cognito/main.tf
@@ -0,0 +1,28 @@
+resource "aws_cognito_user_pool" "this" {
+  name = var.user_pool_name
+  username_attributes = var.username_attributes
+  email_configuration {
+    email_sending_account = var.email_sending_account
+  }
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
+    string_attribute_constraints {
+        max_length = "2048"
+        min_length = "0"
+      }
+  }
+  user_attribute_update_settings {
+    attributes_require_verification_before_update = [
+        "email",
+      ]
+  }
+  auto_verified_attributes = var.username_attributes
+  username_configuration {
+    case_sensitive = false
+  }
+  deletion_protection = var.deletion_protection ? "ACTIVE" : "INACTIVE"
+}
